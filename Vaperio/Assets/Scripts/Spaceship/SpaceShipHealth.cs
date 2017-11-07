cb1fee6a8c7e0a68811d8139952691a3a2974bb9
Daniel Hernandez <dhern001@cgs-macd-15186.campus.goldsmiths.ac.uk>
2017-11-07 16:54:19 +0000

    Initial commit with unity code

---
+++ Vaperio/Assets/Scripts/Spaceship/SpaceShipHealth.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShipHealth : MonoBehaviour {
+    
+    private int startingHealth;
+    private int currentHealth;
+    private Color damageColor = new Color(255f,255f,255f, 1f);
+    private SpriteRenderer spaceshipRenderer;
+
+
+	// Use this for initialization
+	void Start () {
+        this.startingHealth    = 3;
+        this.spaceshipRenderer = GetComponent<SpriteRenderer>();
+        //spaceshipRenderer.color = Color.blue;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Vaperio/Assets/Scripts/Spaceship/SpaceShipInput.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShipInput : MonoBehaviour {
+
+    private Rigidbody2D spaceshipBody;
+
+    
+	// Use this for initialization
+	void Start () {
+        this.spaceshipBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        handleMovement();
+        handleShooting();
+	}
+    
+    void handleMovement() {
+        //Deal with rotation first
+        float rotationSpeed = 200f;
+        Vector2 speed = new Vector2( 0f, 10.0F);
+		float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+		rotation *= Time.deltaTime;
+		transform.Rotate(0, 0 , -rotation);
+        
+		//Translation is an special case, only occurs if the player wants to go up!
+		if( Input.GetAxis("Vertical") > 0 ){
+			Vector2 translation = Input.GetAxis("Vertical") * speed;
+			translation *= Time.deltaTime;
+			spaceshipBody.AddRelativeForce(translation, ForceMode2D.Impulse);
+		}
+    }
+    
+    void handleShooting() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            shootBullet();
+        }
+    }
+    
+    void shootBullet() {
+        
+    }
+    
+    
+}

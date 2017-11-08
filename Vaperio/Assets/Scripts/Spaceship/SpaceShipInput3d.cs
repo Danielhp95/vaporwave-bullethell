350ce379abfe501349db604d31d04824c8647c81
Igor DallAvanzi <idall001@gold.ac.uk>
2017-11-08 16:57:07 +0000

    inputlag added

--- Vaperio/Assets/Scripts/Spaceship/SpaceShipInput3d.cs
+++ Vaperio/Assets/Scripts/Spaceship/SpaceShipInput3d.cs
@@ -6,16 +6,28 @@ public class SpaceShipInput3d : MonoBehaviour {
     
     private Rigidbody spaceshipBody;
     public float VerticalSpeed = 10.0f, HorizontalSpeed=10.0f, DepthSpeed=10.0f;
+    public int lagDur = 10;
+    private int lagInd = 0, lagCount;
+    private float[] hInputLag;
+    
 
 	// Use this for initialization
 	void Start () {
 		this.spaceshipBody = GetComponent<Rigidbody>();
+        this.hInputLag = new float[lagDur];
+        this.lagCount = lagDur-1;
+        for(int i=0; i < lagDur; i = i+1){
+            hInputLag[i]=0.0f;
+        }
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
         handleMovement();
         handleShooting();
+        lagInd = lagInd + 1;
+        if(lagInd == lagCount){lagInd = 0;} 
 		
 	}
 
@@ -25,15 +37,18 @@ public class SpaceShipInput3d : MonoBehaviour {
         Vector3 speedV = new Vector3(-1.0f*(VerticalSpeed),0,0);
         Vector3 speedZ = new Vector3(0,0,DepthSpeed);
         Vector3 translation;
+        hInputLag[lagInd]=0.0f; //comment for crazy effect
     
         if(Input.GetAxis("Horizontal") != 0){
-            translation = Input.GetAxis("Horizontal") * speedH;
+            hInputLag[lagInd]=Input.GetAxis("Horizontal");
+            translation = hInputLag[lagCount-lagInd] * speedH;
             translation *= Time.deltaTime;
             spaceshipBody.AddRelativeForce(translation, ForceMode.Impulse);
         }
         
         if(Input.GetAxis("Vertical") != 0){
-            translation = Input.GetAxis("Vertical") * speedV;
+            hInputLag[lagInd]=Input.GetAxis("Vertical");
+            translation = hInputLag[lagCount-lagInd] * speedV;
             translation *= Time.deltaTime;
             spaceshipBody.AddRelativeForce(translation, ForceMode.Impulse);
         }

101b749cd89b34799e12da3b10a0386121dfbeb9
Pete Y <pyork31@googlemail.com>
2017-11-09 14:47:16 +0000

    Add scrolling background and ship to simpsonswave level

---
+++ Vaperio/Assets/Scripts/World/BackgroundSroller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundSroller : MonoBehaviour {
+	public float speed = 0.005f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 translation = new Vector2 (speed * Time.deltaTime, 0f);
+		var material = GetComponent<Renderer>().material;
+		material.mainTextureOffset =  translation + material.mainTextureOffset;
+	}
+}

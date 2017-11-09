0f6b9f5a30170819697cf559c479ec0761eabf15
Igor DallAvanzi <idall001@gold.ac.uk>
2017-11-09 20:29:51 +0000

    ralph shooting choo projectiles, player gets damaged

---
+++ Vaperio/Assets/Scripts/Enemies/Ralph.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ralph : MonoBehaviour {
+    
+    public GameObject bullet;
+    private EnemyShoot shoot;
+    private float timer;
+    public float shootingTime = 0.8f;
+    
+
+	// Use this for initialization
+	void Start () {
+        EnemyShoot shoot = this.GetComponent<EnemyShoot>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if(timer > shootingTime){
+            shootBullet();
+            timer=0;
+        }
+		
+	}
+    
+    void shootBullet() {
+        Vector3 offset = new Vector3(-0.8f,0f,0f); 
+        Instantiate(bullet, this.transform.position + offset, Quaternion.identity);
+    }
+}
--- Vaperio/Assets/Scripts/Spaceship/SpaceShipHealth.cs
+++ Vaperio/Assets/Scripts/Spaceship/SpaceShipHealth.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SpaceShipHealth : MonoBehaviour {
     
-    private int startingHealth;
+    public  int startingHealth = 30;
     private int currentHealth;
     private Color damageColor = new Color(255f,255f,255f, 1f);
     private SpriteRenderer spaceshipRenderer;
@@ -12,13 +12,36 @@ public class SpaceShipHealth : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        this.startingHealth    = 3;
+        currentHealth = startingHealth;
         this.spaceshipRenderer = GetComponent<SpriteRenderer>();
         //spaceshipRenderer.color = Color.blue;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        
 		
 	}
+    
+    
+    
+    void OnTriggerEnter(Collider other) {
+        if(other.gameObject.layer==11){
+            BulletMovement bullet =  other.GetComponent<BulletMovement>();
+            ApplyDamage(bullet.bulletDamage);
+            Destroy(other.gameObject);
+            }
+        }
+        
+    
+    private void ApplyDamage (int damage){
+        currentHealth -= damage;
+        CheckForDeath();
+    }
+    
+    private void CheckForDeath() {
+        if(currentHealth <= 0){
+            Destroy(this.gameObject);
+         }
+     }
 }

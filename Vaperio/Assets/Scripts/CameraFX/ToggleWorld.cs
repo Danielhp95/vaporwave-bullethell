acfe08d70065e7d42ff96a0079da79c268134880
Pete Y <pyork31@googlemail.com>
2017-11-09 10:39:56 +0000

    Functioning Nether World Flip

--- Vaperio/Assets/Scripts/CameraFX/ToggleWorld.cs
+++ Vaperio/Assets/Scripts/CameraFX/ToggleWorld.cs
@@ -9,11 +9,17 @@ public class ToggleWorld : MonoBehaviour {
 	private bool returning = false;
 	private float startingPosition = -9f;
 	private AmbientCameraMovements ambientCameraMovements;
+	private FlipWorld backgroundFlipWorld;
+	private FlipWorld foregroundFlipWorld;
 
 	// Use this for initialization
 	void Start () {
 		GameObject camera = GameObject.Find ("Main Camera");
 		ambientCameraMovements = camera.GetComponent<AmbientCameraMovements> ();
+		GameObject background = GameObject.Find ("Background");
+		backgroundFlipWorld = background.GetComponent<FlipWorld> ();
+		GameObject foreground = GameObject.Find ("Foreground");
+		foregroundFlipWorld = foreground.GetComponent<FlipWorld> ();
 	}
 
 	// Update is called once per frame
@@ -43,6 +49,8 @@ public class ToggleWorld : MonoBehaviour {
 	private void checkForReverse() {
 		if (transform.position.z > 0f) {
 			speed = -speed;
+			backgroundFlipWorld.flipWorld ();
+			foregroundFlipWorld.flipWorld ();
 			transform.Translate (0f, 0f, -2 * transform.position.z);
 			returning = true;
 		}

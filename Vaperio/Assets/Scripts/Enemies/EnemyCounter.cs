8eb6dfb0534a7023150511a1698793d6a4eb9494
Pete Y <pyork31@googlemail.com>
2017-11-13 14:41:04 +0000

    Add a ralph counter

---
+++ Vaperio/Assets/Scripts/Enemies/EnemyCounter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCounter : MonoBehaviour {
+	public int enemiesKilled = 0;
+	private TextMesh textMesh;
+
+	// Use this for initialization
+	void Start () {
+		textMesh = gameObject.GetComponent<TextMesh> ();		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void enemyKilled() {
+		enemiesKilled++;
+		textMesh.text = "Ralphs Bullied: " + enemiesKilled;
+	}
+}
--- Vaperio/Assets/Scripts/Enemies/EnemyHealth.cs
+++ Vaperio/Assets/Scripts/Enemies/EnemyHealth.cs
@@ -7,10 +7,12 @@ public class EnemyHealth : MonoBehaviour {
     public bool isNether = false;
     public int startingHealth = 100; 
     private int currentHealth;
+	private EnemyCounter enemyCounter;
 
 	// Use this for initialization
 	void Start () {
         currentHealth = startingHealth;
+		enemyCounter = GameObject.Find ("RalphCounter").GetComponent<EnemyCounter>();
 	}
 	
 	// Update is called once per frame
@@ -36,6 +38,7 @@ public class EnemyHealth : MonoBehaviour {
     private void CheckForDeath() {
         if(currentHealth <= 0){
             Destroy(this.gameObject);
+			enemyCounter.enemyKilled ();
          }
      }
         

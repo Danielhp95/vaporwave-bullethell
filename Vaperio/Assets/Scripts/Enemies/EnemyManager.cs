eb54abf69e756f34b8ddbce430145031af96c758
Pete Y <pyork31@googlemail.com>
2017-11-13 11:56:58 +0000

    Fix spawning into the netherworld

--- Vaperio/Assets/Scripts/Enemies/EnemyManager.cs
+++ Vaperio/Assets/Scripts/Enemies/EnemyManager.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 public class EnemyManager : MonoBehaviour
 {
     private SpaceShipHealth spaceShipHealth; // Reference to the player's health.
-    public GameObject enemy;                // The enemy prefab to be spawned.
+	public GameObject enemy;                // The enemy prefab to be spawned.
+	public GameObject netherEnemy;                // The enemy prefab to be spawned.
     public float spawnTime = 3f;            // How long between each spawn.
-    private Transform foregroundTransform;
-    public Vector3 minSpawnValues;
-    public Vector3 maxSpawnValues;
+	private GameObject foreground;
+	public Vector3 minSpawnValues;
+	public Vector3 maxSpawnValues;
 
     void Start ()
     {
         
-        foregroundTransform = GameObject.Find("Foreground").transform;
+        foreground = GameObject.Find("Foreground");
         InvokeRepeating ("Spawn", spawnTime, spawnTime);
     }
 
@@ -29,9 +30,16 @@ public class EnemyManager : MonoBehaviour
         }
         */
 
-        GameObject spawnedEnemy =  Instantiate (enemy, generatePoint(), foregroundTransform.rotation, foregroundTransform);
-        spawnedEnemy.transform.Rotate(0,180,0);
+		GameObject toSpawn = shouldBeNether() ? netherEnemy : enemy;
+		GameObject spawnedEnemy =  Instantiate (toSpawn, new Vector3(0f, 0f, 0f), foreground.transform.rotation, foreground.transform);
+		spawnedEnemy.transform.Rotate (0, 180, 0);
+		spawnedEnemy.transform.Translate (generatePoint ());
+		spawnedEnemy.transform.Translate (new Vector3(0f, 0f, -4f), Space.World);
     }
+
+	private bool shouldBeNether() {
+		return Random.Range (0, 2) == 1;
+	}
     
     private Vector3 generatePoint(){
         float x = (float) Random.Range (minSpawnValues.x, maxSpawnValues.x) ;

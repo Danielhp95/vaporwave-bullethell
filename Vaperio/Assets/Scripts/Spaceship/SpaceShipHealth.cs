827e605ca8de8a93d16eeea470d456ba853c0292
Pete Y <pyork31@googlemail.com>
2017-11-14 11:24:09 +0000

    IncreaseCameraSpeed when health goes low

--- Vaperio/Assets/Scripts/Spaceship/SpaceShipHealth.cs
+++ Vaperio/Assets/Scripts/Spaceship/SpaceShipHealth.cs
@@ -7,20 +7,40 @@ public class SpaceShipHealth : MonoBehaviour {
     
     public  int startingHealth = 30;
     public int currentHealth;
-    private Color damageColor = new Color(255f,255f,255f, 1f);
-    private SpriteRenderer spaceshipRenderer;
+	private Color normalColor = Color.white;
+	private Color hitColour = Color.red;
+	private float timeToNormal = 0.2f;
+	private float timeSinceHit = 0.2f;
+	private List<SpriteRenderer> spriteRenderers = new List<SpriteRenderer>();
 
 
 	// Use this for initialization
 	void Start () {
         currentHealth = startingHealth;
-        this.spaceshipRenderer = GetComponent<SpriteRenderer>();
+		GetSpriteRenderers ();
+	}
+
+	private void GetSpriteRenderers() {
+		spriteRenderers.Add(gameObject.GetComponent<SpriteRenderer> ());
+		List<SpriteRenderer> children = new List<SpriteRenderer> (gameObject.GetComponentsInChildren<SpriteRenderer> ());
+		spriteRenderers.AddRange (children);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        
-		
+		UpdateColour ();
+	}
+
+	private void UpdateColour () {
+		if (timeSinceHit < timeToNormal) {
+			timeSinceHit += Time.deltaTime;
+		}
+		if (timeSinceHit > timeToNormal) {
+			foreach (SpriteRenderer spriteRenderer in spriteRenderers) {
+				spriteRenderer.color = normalColor;
+			}
+		}
 	}
     
     
@@ -36,13 +56,31 @@ public class SpaceShipHealth : MonoBehaviour {
     
     private void ApplyDamage (int damage){
         currentHealth -= damage;
-        CheckForDeath();
+		timeSinceHit = 0f;
+		CheckForDeath();
+		SetHitColour ();
     }
     
     private void CheckForDeath() {
-        if(currentHealth <= 0){
-            Destroy(this.gameObject);
+		if (currentHealth <= 0) {
+			Destroy (this.gameObject);
 			SceneManager.LoadScene ("menu");
-         }
-     }
+		} else if (currentHealth <= 10) {
+			IncreaseCameraSpeed ();
+		}
+	}
+
+	private void IncreaseCameraSpeed() {
+		AmbientCameraMovements ambientMovement = GameObject.Find ("Main Camera").GetComponent<AmbientCameraMovements> ();
+		ambientMovement.spaceSpeed = ambientMovement.spaceSpeed * 2.5f;
+		ambientMovement.postionWidths = ambientMovement.postionWidths * 2f;
+		ambientMovement.rotationSpeed = ambientMovement.rotationSpeed * 2.5f;
+		ambientMovement.rotationWidths = ambientMovement.rotationWidths * 2f;
+	}
+
+	private void SetHitColour () {
+		foreach (SpriteRenderer spriteRenderer in spriteRenderers) {
+			spriteRenderer.color = hitColour;
+		}
+	}
 }

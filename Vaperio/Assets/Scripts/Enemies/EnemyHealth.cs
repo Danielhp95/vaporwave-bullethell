c4562d1badd57061c459c2d9be1c8f057c57b05f
Pete Y <pyork31@googlemail.com>
2017-11-14 10:49:47 +0000

    Add visual feedback to ralpg being shot

--- Vaperio/Assets/Scripts/Enemies/EnemyHealth.cs
+++ Vaperio/Assets/Scripts/Enemies/EnemyHealth.cs
@@ -8,19 +8,43 @@ public class EnemyHealth : MonoBehaviour {
     public int startingHealth = 100; 
     private int currentHealth;
 	private EnemyCounter enemyCounter;
+	private Color normalColor = Color.white;
+	private Color hitColour = Color.red;
+	private float timeToNormal = 0.2f;
+	private float timeSinceHit = 0.2f;
+	private List<SpriteRenderer> spriteRenderers = new List<SpriteRenderer>();
 
 	// Use this for initialization
 	void Start () {
         currentHealth = startingHealth;
 		enemyCounter = GameObject.Find ("RalphCounter").GetComponent<EnemyCounter>();
+		GetSpriteRenderers ();
+	}
+
+	private void GetSpriteRenderers() {
+		spriteRenderers.Add(gameObject.GetComponent<SpriteRenderer> ());
+		List<SpriteRenderer> children = new List<SpriteRenderer> (gameObject.GetComponentsInChildren<SpriteRenderer> ());
+		spriteRenderers.AddRange (children);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        	
+		UpdateColour ();
+	}
+
+	private void UpdateColour () {
+		if (timeSinceHit < timeToNormal) {
+			timeSinceHit += Time.deltaTime;
+		}
+		if (timeSinceHit > timeToNormal) {
+			foreach (SpriteRenderer spriteRenderer in spriteRenderers) {
+				spriteRenderer.color = normalColor;
+			}
+		}
 	}
     
-    void OnTriggerEnter(Collider other) {
+    void OnTriggerEnter (Collider other) {
 		if(other.gameObject.layer==10){
             BulletMovement bullet =  other.GetComponent<BulletMovement>();
 			if (bullet.isNether == isNether) {
@@ -32,17 +56,23 @@ public class EnemyHealth : MonoBehaviour {
     
     private void ApplyDamage (int damage){
         currentHealth -= damage;
-        CheckForDeath();
+		timeSinceHit = 0f;
+        CheckForDeath ();
+		SetHitColour ();
     }
     
-    private void CheckForDeath() {
+    private void CheckForDeath () {
         if(currentHealth <= 0){
             Destroy(this.gameObject);
 			enemyCounter.enemyKilled ();
          }
      }
         
-        
+	private void SetHitColour () {
+		foreach (SpriteRenderer spriteRenderer in spriteRenderers) {
+			spriteRenderer.color = hitColour;
+		}
+	}
 }
 
     

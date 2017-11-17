3431a61134138ef76a00e46f53d511081aac5cc5
Pete Y <pyork31@googlemail.com>
2017-11-17 11:20:19 +0000

    Remove some debugging

--- Vaperio/Assets/Scripts/Enemies/EnemyManager.cs
+++ Vaperio/Assets/Scripts/Enemies/EnemyManager.cs
@@ -45,9 +45,6 @@ public class EnemyManager : MonoBehaviour
 	private bool shouldSpawn() {
 		float timeSinceLastSpawnSquared = timeSinceLastSpawn * timeSinceLastSpawn;
 		int toBeat = Random.Range (0, 100);
-		print ("timeSinceLastSpawnSquared - " + timeSinceLastSpawnSquared);
-		print (Time.time * 0.07f);
-		print ("tobeat - " + toBeat);
 		timeSinceLastSpawnSquared += Time.time * 0.09f;
 		return timeSinceLastSpawnSquared > toBeat && !Pause.paused;
 	}

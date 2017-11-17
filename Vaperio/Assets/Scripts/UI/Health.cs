dddc4f1ffcc49ca693495df20aea5bec44834920
Pete Y <pyork31@googlemail.com>
2017-11-17 10:23:37 +0000

    Add health UI

---
+++ Vaperio/Assets/Scripts/UI/Health.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour {
+	Text text;
+	SpaceShipHealth spaceShipHealth;
+
+	void Start () {
+		spaceShipHealth = GameObject.Find ("spaceship3D").GetComponent<SpaceShipHealth> ();
+		text = gameObject.GetComponent<Text> ();;
+	}
+
+	void Update () {
+		int health = spaceShipHealth.currentHealth;
+		text.text = "Your health:" + health;	
+	}
+}
